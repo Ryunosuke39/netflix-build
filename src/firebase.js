57acc8a6ef2063224bd6b22eb132b8a7c127408b
Ryunosuke M <111721351+Ryunosuke39@users.noreply.github.com>
2024-05-12 00:00:29 +1000

    for creating api key

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8yPobXooeTUI3J9n6R4IoslWtyd5hdKc",
+    authDomain: "netflix-build-b55f0.firebaseapp.com",
+    projectId: "netflix-build-b55f0",
+    storageBucket: "netflix-build-b55f0.appspot.com",
+    messagingSenderId: "726354310128",
+    appId: "1:726354310128:web:d799e2467a12e6de376335"
+  };
